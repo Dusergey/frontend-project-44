5ea36f9010e8aa20ba3e7da097b0cff3c7b51714
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-10 19:06:32 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,4 @@
-/* eslint-disable @stylistic/semi */
 #!/usr/bin/env node
-console.log('DEBUG: brain-progression started');
 import startProgressionGame from '../src/games/progression.js';
 
 startProgressionGame();
