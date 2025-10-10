50a1228bd28ad62bfbaf3d2b8eee4fa415374127
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-10 19:36:14 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,20 +1,20 @@
-import launchGame from '../index.js';
-import getRandomNumber from '../random.js';
+import launchGame from '../index.js'
+import getRandomNumber from '../random.js'
 
 // Число четное?
-const isEven = (num) => num % 2 === 0;
+const isEven = (num) => num % 2 === 0
 
 // Функция, которая генерирует вопрос для игры в четность числа
 const generateQuestionParity = () => {
   // Создается случайный число
-  const number = getRandomNumber(100);
+  const number = getRandomNumber(100)
   // Получаем правильный ответ на созданный вопрос
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
   // Возвращаем из функции созданный вопрос и правильный ответ на него
-  return [number, correctAnswer];
-};
+  return [number, correctAnswer]
+}
 // Правила игры
-const rulesGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+const rulesGame = 'Answer "yes" if the number is even, otherwise answer "no".'
 // Функция запуска игры
-const startEvenGame = () => launchGame(rulesGame, generateQuestionParity);
-export default startEvenGame;
+const startEvenGame = () => launchGame(rulesGame, generateQuestionParity)
+export default startEvenGame
