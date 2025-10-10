de0f53aefb08aa1457444146ffee4ff5c386074c
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-10 19:13:09 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -19,21 +19,20 @@ const getProgression = (num, difference, len) => {
 
 // Функция, которая генерирует вопрос к игре арифметическая прогрессия
 const generateQuestionProgression = () => {
-  // Создаем случайное число, с которого будет начинаться прогрессия
   const number = getRandomNumber(50);
-  // Последовательность от 5 до 10
   const len = getRandomNumber(11, 5);
-  // Получаем индекс, по которому будет пропущено число
   const skip = getRandomNumber(len);
-  // Создаем случайное число для разности арифметической прогрессии
   const difference = getRandomNumber(21, -20);
-  // Создаем арифметическую прогрессию
   const progression = getProgression(number, difference, len);
-  // Правильный ответ на вопрос
   const correctAnswer = String(progression[skip]);
-  // Создаем вопрос
-  const question = String(progression).replaceAll(',', ' ').replace(correctAnswer, '..', 1);
-  // Возвращаем из функции созданный вопрос и правильный ответ на него
+  
+  // Создаем копию прогрессии и заменяем элемент по индексу
+  const progressionWithHidden = [...progression];
+  progressionWithHidden[skip] = '..';
+  
+  // Преобразуем в строку с пробелами
+  const question = progressionWithHidden.join(' ');
+  
   return [question, correctAnswer];
 };
 // Правила игры
