84f1d0432a0c12f0c6c450897473cad890432cb5
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-10 19:31:47 +0300

    Update random.js

--- src/random.js
+++ src/random.js
@@ -1,5 +1,5 @@
 // Функция, которая генерирует случайные числа в заданном диапазоне
 // Функция, которая создает число в заданном диапазоне [ x, y )
-const getRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const getRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min) + min)
 
-export default getRandomNumber;
+export default getRandomNumber
