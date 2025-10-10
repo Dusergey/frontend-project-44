29283c7db98f1ee69f01482c08447e514a07c579
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-10 19:37:52 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,28 +1,28 @@
-import launchGame from '../index.js';
-import getRandomNumber from '../random.js';
+import launchGame from '../index.js'
+import getRandomNumber from '../random.js'
 
 // Число простое?
 const isPrime = (num) => {
   for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
-      return false;
+      return false
     }
   }
 
-  return true;
-};
+  return true
+}
 
 // Функция, которая генерирует вопрос к игре простое число
 const generateQuestionPrime = () => {
   // Создаем случайное число
-  const number = getRandomNumber(200);
+  const number = getRandomNumber(200)
   // Правильный ответ на вопрос
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  const correctAnswer = isPrime(number) ? 'yes' : 'no'
   // Возвращаем из функции созданный вопрос и правильный ответ на него
-  return [number, correctAnswer];
-};
+  return [number, correctAnswer]
+}
 // Правила игры
-const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 // Функция запуска игры
-const startPrimeGame = () => launchGame(rulesGame, generateQuestionPrime);
-export default startPrimeGame;
+const startPrimeGame = () => launchGame(rulesGame, generateQuestionPrime)
+export default startPrimeGame
